1155c60fc26c7b58511e8b3340d0059e6e17041d
shizuku86 <shizuku86710@gmail.com>
2025-09-25 12:53:02 +0900

    make WerewolfGameManager & RoleRegistrationReceiver

---
+++ scripts/GameManager/game/WerewolfGameManager.ts
@@ -0,0 +1,16 @@
+import { RoleRegistrationReceiver } from "./outgame/RoleRegistrationReceiver";
+
+export class WerewolfGameManager {
+    private readonly roleRegistrationReceiver: RoleRegistrationReceiver;
+    private constructor() {
+        this.roleRegistrationReceiver = RoleRegistrationReceiver.create(this);
+    }
+    private static instance: WerewolfGameManager | null = null;
+
+    public static getInstance(): WerewolfGameManager {
+        if (this.instance === null) {
+            this.instance = new WerewolfGameManager();
+        }
+        return this.instance;
+    }
+}
---
+++ scripts/GameManager/game/outgame/RoleRegistrationReceiver.ts
@@ -0,0 +1,8 @@
+import type { WerewolfGameManager } from "../WerewolfGameManager";
+
+export class RoleRegistrationReceiver {
+    private constructor(werewolfGameManager: WerewolfGameManager) {}
+    public static create(werewolfGameManager: WerewolfGameManager): RoleRegistrationReceiver {
+        return new RoleRegistrationReceiver(werewolfGameManager);
+    }
+}
