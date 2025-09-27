dadb579646931f241b554e62a428c667e2b8bc02
shizuku86 <shizuku86710@gmail.com>
2025-09-28 04:51:33 +0900

    rename to Register

---
+++ scripts/GameManager/game/outgame/RoleRegister.ts
@@ -0,0 +1,28 @@
+import { ConsoleManager } from "../../../Kairo/utils/ConsoleManager";
+import type { Role } from "../../data/roles";
+import { WerewolfGameManager } from "../WerewolfGameManager";
+
+export class RoleRegister {
+    private constructor(private readonly werewolfGameManager: WerewolfGameManager) {}
+    public static create(werewolfGameManager: WerewolfGameManager): RoleRegister {
+        return new RoleRegister(werewolfGameManager);
+    }
+
+    public registrationRoles(args: string[]): void {
+        const roles: Role[] = args.slice(1).map((arg: string) =>{
+            let data;
+            try {
+                data = JSON.parse(arg);
+            } catch (e) {
+                ConsoleManager.error("Failed to parse role registration data: Invalid JSON format.");
+            }
+
+            if (data && WerewolfGameManager.getInstance().isRole(data)) {
+                return data as Role;
+            }
+            return null;
+        }).filter((role): role is Role => role !== null);
+
+        this.werewolfGameManager.setRoles(args[0] as string, roles);
+    }
+}
