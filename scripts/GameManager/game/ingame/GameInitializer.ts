b9a40c56e74a1a635a772795a9c23d4441f74b1c
shizuku86 <shizuku86710@gmail.com>
2025-11-05 15:15:37 +0900

    create GameInitializer

---
+++ scripts/GameManager/game/ingame/GameInitializer.ts
@@ -0,0 +1,8 @@
+import type { WerewolfGameManager } from "../WerewolfGameManager";
+
+export class GameInitializer {
+    private constructor(private readonly werewolfGameManager: WerewolfGameManager) {}
+    public static create(werewolfGameManager: WerewolfGameManager): GameInitializer {
+        return new GameInitializer(werewolfGameManager);
+    }
+}
