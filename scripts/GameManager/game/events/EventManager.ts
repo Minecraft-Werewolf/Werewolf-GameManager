c1966600b51becc5e7ba3a589e662e455f2b1136
shizuku86 <shizuku86710@gmail.com>
2025-11-06 14:11:59 +0900

    added event

--- scripts/GameManager/game/events/EventManager.ts
+++ scripts/GameManager/game/events/EventManager.ts
@@ -1,11 +1,14 @@
 import type { WerewolfGameManager } from "../WerewolfGameManager";
 import { ItemUseHandler } from "./ItemUseHandler";
+import { ScriptEventReceiveHandler } from "./ScriptEventReceiveHandler";
 
 export class EventManager {
     private readonly itemUse: ItemUseHandler;
+    private readonly scriptEventReceive: ScriptEventReceiveHandler;
 
     private constructor(private readonly werewolfGameManager: WerewolfGameManager) {
         this.itemUse = ItemUseHandler.create(this);
+        this.scriptEventReceive = ScriptEventReceiveHandler.create(this);
     }
     public static create(werewolfGameManager: WerewolfGameManager): EventManager {
         return new EventManager(werewolfGameManager);
@@ -13,10 +16,12 @@ export class EventManager {
 
     public subscribeAll(): void {
         this.itemUse.subscribe();
+        this.scriptEventReceive.subscribe();
     }
 
     public unsubscribeAll(): void {
         this.itemUse.unsubscribe();
+        this.scriptEventReceive.unsubscribe();
     }
 
     public getWerewolfGameManager(): WerewolfGameManager {
