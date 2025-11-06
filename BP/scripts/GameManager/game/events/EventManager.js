f7e48392d70447905fa3470af185cba4ed468a22
shizuku86 <shizuku86710@gmail.com>
2025-11-06 14:12:10 +0900

    build

--- BP/scripts/GameManager/game/events/EventManager.js
+++ BP/scripts/GameManager/game/events/EventManager.js
@@ -1,17 +1,21 @@
 import { ItemUseHandler } from "./ItemUseHandler";
+import { ScriptEventReceiveHandler } from "./ScriptEventReceiveHandler";
 export class EventManager {
     constructor(werewolfGameManager) {
         this.werewolfGameManager = werewolfGameManager;
         this.itemUse = ItemUseHandler.create(this);
+        this.scriptEventReceive = ScriptEventReceiveHandler.create(this);
     }
     static create(werewolfGameManager) {
         return new EventManager(werewolfGameManager);
     }
     subscribeAll() {
         this.itemUse.subscribe();
+        this.scriptEventReceive.subscribe();
     }
     unsubscribeAll() {
         this.itemUse.unsubscribe();
+        this.scriptEventReceive.unsubscribe();
     }
     getWerewolfGameManager() {
         return this.werewolfGameManager;
