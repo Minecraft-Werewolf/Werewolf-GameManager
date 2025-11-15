05fda810df687f85c05f7f97c3a038dccff62ce0
KKaill <207049580+KKaill@users.noreply.github.com>
2025-11-16 00:30:50 +0900

    Update HostManager.ts

--- scripts/GameManager/system/HostManager.ts
+++ scripts/GameManager/system/HostManager.ts
@@ -1,16 +1,14 @@
 import type { Player } from "@minecraft/server";
 import type { SystemManager } from "../game/SystemManager";
 import { ModalFormData } from "@minecraft/server-ui";
-
-const settingsUI = new ModalFormData().title("");
-
 export class HostManager {
-    private constructor(public readonly systemManager: SystemManager) {}
+    #settingsUI = new ModalFormData().title("");
+    private constructor(private readonly systemManager: SystemManager) {}
     public static create(systemManager: SystemManager): HostManager {
         return new HostManager(systemManager);
     }
     public showSettingsMenu(player: Player) {
-        settingsUI.show(player).then((re) => {
+        this.#settingsUI.show(player).then((re) => {
             const { formValues, canceled, cancelationReason } = re;
         });
     }
