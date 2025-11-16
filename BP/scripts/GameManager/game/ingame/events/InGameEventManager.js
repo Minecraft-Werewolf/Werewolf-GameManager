0a89fc3ae518aee42e6e7f8a5bfbbb730938aa4f
shizuku86 <shizuku86710@gmail.com>
2025-11-16 20:54:59 +0900

    build

--- BP/scripts/GameManager/game/ingame/events/InGameEventManager.js
+++ BP/scripts/GameManager/game/ingame/events/InGameEventManager.js
@@ -1,19 +1,23 @@
 import { BaseEventManager } from "../../events/BaseEventManager";
 import { InGameEntityHurtHandler } from "./EntityHurt";
+import { InGameItemUseHandler } from "./ItemUse";
 export class InGameEventManager extends BaseEventManager {
     constructor(inGameManager) {
         super();
         this.inGameManager = inGameManager;
         this.entityHurt = InGameEntityHurtHandler.create(this);
+        this.itemUse = InGameItemUseHandler.create(this);
     }
     static create(inGameManager) {
         return new InGameEventManager(inGameManager);
     }
     subscribeAll() {
         this.entityHurt.subscribe();
+        this.itemUse.subscribe();
     }
     unsubscribeAll() {
         this.entityHurt.unsubscribe();
+        this.itemUse.unsubscribe();
     }
     getInGameManager() {
         return this.inGameManager;
