b2857c96e044d5e62efa3549a658d5566ca9365d
Himapoa <honishizawa123@icloud.com>
2026-02-09 17:44:39 +0900

    弓を打って殺した時にサウンドがなるようにした

--- scripts/GameManager/game/ingame/events/InGameEventManager.ts
+++ scripts/GameManager/game/ingame/events/InGameEventManager.ts
@@ -4,18 +4,21 @@ import { InGameEntityHurtHandler } from "./EntityHurt";
 import { InGameItemUseHandler } from "./ItemUse";
 import { InGamePlayerLeaveHandler } from "./PlayerLeave";
 import { InGameProjectileHitBlockHandler } from "./ProjectileHitBlock";
+import { InGameProjectileHitEntityHandler } from "./ProjectileHitEntity";
 
 export class InGameEventManager extends BaseEventManager {
     private entityHurt: InGameEntityHurtHandler;
     private itemUse: InGameItemUseHandler;
     private playerLeave: InGamePlayerLeaveHandler;
     private projectileHitBlock: InGameProjectileHitBlockHandler;
+    private projectileHitEntity: InGameProjectileHitEntityHandler;
     private constructor(private readonly inGameManager: InGameManager) {
         super();
         this.entityHurt = InGameEntityHurtHandler.create(this);
         this.itemUse = InGameItemUseHandler.create(this);
         this.playerLeave = InGamePlayerLeaveHandler.create(this);
         this.projectileHitBlock = InGameProjectileHitBlockHandler.create(this);
+        this.projectileHitEntity = InGameProjectileHitEntityHandler.create(this);
     }
 
     public static create(inGameManager: InGameManager): InGameEventManager {
@@ -27,6 +30,7 @@ export class InGameEventManager extends BaseEventManager {
         this.itemUse.subscribe();
         this.playerLeave.subscribe();
         this.projectileHitBlock.subscribe();
+        this.projectileHitEntity.subscribe();
     }
 
     public override unsubscribeAll(): void {
@@ -34,6 +38,7 @@ export class InGameEventManager extends BaseEventManager {
         this.itemUse.unsubscribe();
         this.playerLeave.unsubscribe();
         this.projectileHitBlock.unsubscribe();
+        this.projectileHitEntity.unsubscribe();
     }
 
     public getInGameManager(): InGameManager {
